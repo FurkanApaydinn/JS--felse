b9d80fc2eb44fafddb9048c66989b56fbef03d65
Furkan Apaydın <114331714+FurkanApaydinn@users.noreply.github.com>
2022-11-23 20:46:50 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -18,8 +18,8 @@
 //challange 5
 // const ülke = "Portekiz";
 // const kıta = "Avrupa";
-let nüfus = 40;
-let ülke = "İtalya";
+// let nüfus = 40;
+// let ülke = "İtalya";
 // let dil = "türkçe";
 
 // const adaMı = false
@@ -62,10 +62,10 @@ let ülke = "İtalya";
 //   console.log(`italyanın nüfusu ortalamanın, ${nüfus} milyon altında`);
 // }
 
-const boymurat = 1.75;
-const boyengin = 1.82;
-const ağırlıkmurat = 70;
-const ağırlıkengin = 80;
+// const boymurat = 1.75;
+// const boyengin = 1.82;
+// const ağırlıkmurat = 70;
+// const ağırlıkengin = 80;
 // console.log(
 //   "Murat'ın boyu " + boymurat,
 //   "ve kilosu " +
@@ -77,22 +77,22 @@ const ağırlıkengin = 80;
 //     "ve kilosu " +
 //     ağırlıkengin
 // );
-const vücutkitleendeksimurat = ağırlıkmurat / boymurat ** 2;
-console.log("Muratın vücut kitle endeksi : " + vücutkitleendeksimurat);
-const vücutkitleendeksiengin = ağırlıkengin / boyengin ** 2;
-console.log("Enginin vücut kitle endeksi : " + vücutkitleendeksiengin);
-const büyükmü = vücutkitleendeksimurat > vücutkitleendeksiengin;
-console.log(büyükmü);
-if (vücutkitleendeksiengin > vücutkitleendeksimurat) {
-  console.log(
-    `Enginin ${vücutkitleendeksiengin}'si Muratın ${vücutkitleendeksimurat} daha büyük`
-  );
-} else {
-  console.log(
-    `Muratın ${vücutkitleendeksimurat}'si Engininin ${vücutkitleendeksiengin} daha büyük`
-  );
-}
-
+// const vücutkitleendeksimurat = ağırlıkmurat / boymurat ** 2;
+// console.log("Muratın vücut kitle endeksi : " + vücutkitleendeksimurat);
+// const vücutkitleendeksiengin = ağırlıkengin / boyengin ** 2;
+// console.log("Enginin vücut kitle endeksi : " + vücutkitleendeksiengin);
+// const büyükmü = vücutkitleendeksimurat > vücutkitleendeksiengin;
+// console.log(büyükmü);
+// if (vücutkitleendeksiengin > vücutkitleendeksimurat) {
+//   console.log(
+//     `Enginin ${vücutkitleendeksiengin}'si Muratın ${vücutkitleendeksimurat} daha büyük`
+//   );
+// } else {
+//   console.log(
+//     `Muratın ${vücutkitleendeksimurat}'si Engininin ${vücutkitleendeksiengin} daha büyük`
+//   );
+// }
+//---------------------------------------------------------------//
 //Type conversion & Connection
 // const girişyılı = "1986";
 // console.log(Number(girişyılı), girişyılı);
@@ -149,13 +149,127 @@ if (vücutkitleendeksiengin > vücutkitleendeksimurat) {
 //   console.log("Neden 23 değil");
 // }
 
-const komşusayı = Number(prompt("ülkenizin kaç komşusu var"));
-if (komşusayı == 1) {
-  console.log("Sadece 1 komşu");
-} else if (komşusayı > 1) {
-  console.log("1 den fazla komşu");
+// const komşusayı = Number(prompt("ülkenizin kaç komşusu var"));
+// if (komşusayı == 1) {
+//   console.log("Sadece 1 komşu");
+// } else if (komşusayı > 1) {
+//   console.log("1 den fazla komşu");
+// } else {
+//   console.log("komşu yok");
+// }
+
+// const ehliyetvarmi = true; //A
+// const gözleribozukmu = true; //B
+
+// console.log(ehliyetvarmi && gözleribozukmu);// true
+// const ehliyetvarmi = true; //A
+// const gözleribozukmu = false; //B
+
+// console.log(ehliyetvarmi && gözleribozukmu);// false
+// const ehliyetvarmi = true; //A
+// const görüşüvarmı = false; //B
+
+// // console.log(ehliyetvarmi || gözleribozukmu); //or da bir tane true olması yeterli
+// // console.log(!ehliyetvarmi); //false tersine çevir
+
+// const kullanabilirmi = ehliyetvarmi && görüşüvarmı;
+// // if (kullanabilirmi) {
+// //   console.log("murat araç kullanabilir");
+// // } else {
+// //   console.log("Murat araç kullanamaz");
+// // }
+
+// const dinçmi = true; //c
+// console.log(ehliyetvarmi || görüşüvarmı || dinçmi);
+// if (kullanabilirmi) {
+//   console.log("murat araç kullanabilir");
+// } else {
+//   console.log("Murat araç kullanamaz");
+// }
+
+// const ülke = "Portekiz";
+// // const kıta = "Avrupa";
+// let nüfus = 40;
+// // let ülke = "İtalya";
+// let dil = "türkçe";
+// adaMı = true;
+// if (dil === "İngilizce" && nüfus < 50 && !adaMı) {
+//   console.log(`${ülke} de yaşamalısınız`);
+// } else {
+//   console.log(`${ülke} kriterlerinizi karşlılamıyor`);
+// }
+// const kaplanpuan = (96 + 108 + 89) / 3;
+// console.log(kaplanpuan);
+// const kobrapuan = (88 + 91 + 110) / 3;
+// console.log(kobrapuan);
+// const puan = 100;
+
+// if (kaplanpuan > kobrapuan && kaplanpuan >= puan) {
+//   console.log("Kazanan kaplanlar");
+// } else if (kaplanpuan === kobrapuan) {
+//   console.log("Beraber");
+// } else if (kobrapuan > kaplanpuan && kobrapuan >= puan) {
+//   console.log("kazanan kobra");
+// } else {
+//   console.log("puanlar 100 e eşit değil kazanan yok");
+// }
+//challange 24
+// const ulke = "Türkiye";
+// const kıta = "Asya";
+// let nüfus = 33;
+// let dil = "türkçe";
+// console.log(ulke, kıta, nüfus);
+// const adamı = false;
+// // let dil;
+// console.log(typeof adamı);
+// console.log(typeof kıta);
+// console.log(typeof nüfus);
+// // dil = "türkçe";
+// const dil = "türkçe";
+// console.log(nüfus / 2);
+// nüfus++;
+// console.log(nüfus);
+// console.log(nüfus > 6);
+// console.log(nüfus < 33);
+// // const aciklama =
+// //   ulke + kıta + " 'da ve" + nüfus + " Milyon insan" + dil + " Konuşuyor";
+// // console.log(aciklama);
+
+// //challende 25
+// const ağırlıkmurat = 71;
+// const boymurat = 1.87;
+// const ağırlıkengin = 90;
+// const boyengin = 1.66;
+// const BMImurat = (ağırlıkmurat / boymurat) ** 2;
+// const BMIengin = (ağırlıkengin / boyengin) ** 2;
+// const büyükBMI = BMImurat > BMIengin;
+// console.log(BMImurat, BMIengin, büyükBMI);
+
+// if (nüfus > 33) {
+//   console.log(`${nüfus}'nın ortalamanın üzerinde`);
+// }
+// const komşusayı = prompt("ülkenizin kaç komşusu var");
+// if ("2" == 1) {
+//   console.log("sadece bir komşu");
+// } else if (komşusayı > 1) {
+//   console.log("1 den fazla komşu");
+// } else {
+//   console.log("komşu yok");
+// }
+// if (dil === "İngilizce" && nüfus < 50) {
+//   console.log(`${ulke} de yaşamalısın`);
+// } else {
+//   console.log(`${ulke} kriterlerinizi karşılamıyor`);
+// }
+const kaplanlar = (1000 + 108 + 110) / 3;
+const kobralar = (900 + 108 + 110) / 3;
+// console.log(kaplanlar, kobralar);
+if (kaplanlar > kobralar && kaplanlar > 100) {
+  console.log("kazananlar kaplanlar");
+} else if (kaplanlar === kobralar && kaplanlar >= 100 && kobralar > 100) {
+  console.log("eşit");
+} else if (kobralar > kaplanlar && kobralar > 100) {
+  console.log("kazanan kobralar");
 } else {
-  console.log("komşu yok");
+  console.log("kazanan yok");
 }
-
-
